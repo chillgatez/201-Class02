953fe5042eea715b4c2ab0b78fde11a75e8526b2
Kelsee Watts <123975076+chillgatez@users.noreply.github.com>
2023-02-22 10:18:44 -0600

    quiz script bones

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const user = prompt('Hello, friend what is your name?');
+
+alert(`Welcome ${user}! It's nice to meet you.`)
+
+document.querySelector("button").addEventListener("click", () => {
+    //alert(`ouch, ${user}! that hurt`);
+
+    let q1 = prompt('Is this website about Kelsee?');
+    //console.log();
+
+    let q2 = prompt('Is Kelsee from Taiwan?');
+    //console.log();
+
+    let q3 = prompt('Does Kelsee hate music?');
+    //console.log();
+
+    let q4 = prompt('Is Kelsee in Code School?');
+    //console.log();
+    
+    let q5 = prompt('Did Kelsee work for Reebok?');
+    //console.log();
+
+})
